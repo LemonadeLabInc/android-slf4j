7022b72145401f4ba810e0176e060f28c23b4f68
Pier Fumagalli <pier@usrz.com>
2015-10-26 00:33:34 +0900

    Wrong... We NEED to create the injector during "onCreate(...)"

--- src/main/java/de/lemona/android/guice/InjectableActivity.java
+++ src/main/java/de/lemona/android/guice/InjectableActivity.java
@@ -1,5 +1,7 @@
 package de.lemona.android.guice;
 
+import java.util.Arrays;
+
 import com.google.inject.Module;
 
 import android.app.Activity;
@@ -7,19 +9,29 @@ import android.os.Bundle;
 
 public abstract class InjectableActivity extends Activity {
 
-    protected final Injector injector;// = new InjectorWrapper();
+    private volatile Injector injector = null;
+    private final Iterable<? extends Module> modules;
 
     protected InjectableActivity(Module... modules) {
-        this.injector = Injection.createInjector(this, modules);
+        this.modules = modules == null ? null : Arrays.asList(modules);
     }
 
     protected InjectableActivity(Iterable<? extends Module> modules) {
-        this.injector = Injection.createInjector(this, modules);
+        this.modules = modules;
+    }
+
+    protected Injector getInjector() {
+        if (injector == null) throw new IllegalStateException("Injector unavailable");
+        return injector;
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        this.injector.injectMembers(this);
+
+        if (this.injector == null) {
+            this.injector = Injection.createInjector(this, modules);
+            this.injector.injectMembers(this);
+        }
     }
 }
--- src/main/java/de/lemona/android/guice/InjectableService.java
+++ src/main/java/de/lemona/android/guice/InjectableService.java
@@ -1,25 +1,37 @@
 package de.lemona.android.guice;
 
+import java.util.Arrays;
+
 import com.google.inject.Module;
 
 import android.app.Service;
 
 public abstract class InjectableService extends Service {
 
-    protected final Injector injector;
+    private volatile Injector injector = null;
+    private final Iterable<? extends Module> modules;
 
     protected InjectableService(Module... modules) {
-        this.injector = Injection.createInjector(this, modules);
+        this.modules = modules == null ? null : Arrays.asList(modules);
     }
 
     protected InjectableService(Iterable<? extends Module> modules) {
-        this.injector = Injection.createInjector(this, modules);
+        this.modules = modules;
+    }
+
+    protected Injector getInjector() {
+        if (injector == null) throw new IllegalStateException("Injector unavailable");
+        return injector;
     }
 
     @Override
     public void onCreate() {
         super.onCreate();
-        this.injector.injectMembers(this);
+
+        if (this.injector == null) {
+            this.injector = Injection.createInjector(this, modules);
+            this.injector.injectMembers(this);
+        }
     }
 
 }
